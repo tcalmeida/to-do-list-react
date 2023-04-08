2a72cbe6e8f5413df855bc0dfa82d57ea5a1b7db
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-08 15:23:47 -0300

    refactor: improve indentation

--- to-do-app/src/components/Header/styles.ts
+++ to-do-app/src/components/Header/styles.ts
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Title = styled.h1`
-width: 60vw;
-border: 1px solid red;
-margin: 30px 0;
-text-align: center;
-`
+  width: 60vw;
+  border: 1px solid red;
+  margin: 30px 0;
+  text-align: center;
+`;
