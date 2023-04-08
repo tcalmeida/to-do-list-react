923b02c146f4e4de2976f9c8463c0ea827b29ad2
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-08 09:31:41 -0300

    build: create Button component

---
+++ to-do-app/src/components/Button/index.ts
@@ -0,0 +1,12 @@
+
+interface ButtonProps {
+    text: string;
+    color: string
+    action?: () => void
+    }
+
+const Button = (props:ButtonProps) => {
+    return <ButtonDefault color={props.color} onClick={props.action}>{props.text}</ButtonDefault>
+}
+
+export default Button;
