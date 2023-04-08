eb1cbddad6537dccf0ce013235338ba5a78eee17
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-08 09:44:21 -0300

    build: create Button style component

---
+++ to-do-app/src/components/Button/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ButtonDefault = styled.button`
+  border: none;
+  background-color: green;
+  color: #ffff;
+  border-radius: 50px;
+  padding: 5px 10px;
+  cursor: pointer;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 15px ;
+  margin-left: 10px
+`;
