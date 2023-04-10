a988718a1011aeeed00abdb36b3a386a576410c8
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-10 17:29:26 -0300

    build: create handleAddtask function

--- to-do-app/src/App.tsx
+++ to-do-app/src/App.tsx
@@ -1,4 +1,24 @@
+
+
+interface Task {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
 const App = () => {
+  
+  const [titleTask, setTitleTask] = useState<string>(''); //add state to forms
+  const [task, setTask] = useState<Task[]>(getStorageData());
+  const id = uuidv4();
+
+  const handleAddTask = (task: any) => {
+    setTask([...task, { id: id, title: titleTask, done: false }]);
+  };
+  
+  
+  
+  
   return(<></>);
 };
 
