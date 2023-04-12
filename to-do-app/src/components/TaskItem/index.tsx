1d1397fc1fdc3ae9a70cab7460c0d32c2cad5086
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-12 12:32:35 -0300

    build: create TaskItem component

---
+++ to-do-app/src/components/TaskItem/index.tsx
@@ -0,0 +1,23 @@
+import { Item, Task, CheckBox, Span } from './styles';
+
+interface TaskItemProps {
+  id: string;
+  title: string;
+  done: boolean;
+  deleteTask: () => void;
+  handleCheck: () => void;
+}
+
+function TaskItem(props: TaskItemProps) {
+  return (
+    <Item key={props.id}>
+      <CheckBox type="checkbox" checked={props.done} onChange={props.handleCheck} />
+      <Task done={props.done} onDoubleClick={props.handleCheck}>
+        {props.title}
+      </Task>
+      <Span onClick={props.deleteTask}> ❌ </Span>
+    </Item>
+  );
+}
+
+export default TaskItem;
