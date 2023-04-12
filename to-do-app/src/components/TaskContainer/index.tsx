513a833a956e3fa2fb664c739d7c0b91dde7f94e
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-12 12:33:05 -0300

    build: create TaskContainer component

---
+++ to-do-app/src/components/TaskContainer/index.tsx
@@ -0,0 +1,11 @@
+import { TaskList } from "./styles"; 
+
+interface TaskContainerProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+function TaskContainer(props: TaskContainerProps) {
+  return <TaskList>{props.children}</TaskList>;
+}
+
+export default TaskContainer;
