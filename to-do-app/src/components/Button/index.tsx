770a368e6e644778c50445332c9a9de5325d0f58
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-13 15:46:56 -0300

    refactor: improve indentation

--- to-do-app/src/components/Button/index.tsx
+++ to-do-app/src/components/Button/index.tsx
@@ -1,13 +1,17 @@
 import { ButtonDefault } from './styles';
 
 interface ButtonProps {
-    text: string;
-    color: string
-    action?: () => void
-    }
-
-const Button = (props:ButtonProps) => {
-    return <ButtonDefault color={props.color} onClick={props.action}>{props.text}</ButtonDefault>
+  text: string;
+  color: string;
+  action?: () => void;
 }
+
+const Button = (props: ButtonProps) => {
+  return (
+    <ButtonDefault color={props.color} onClick={props.action}>
+      {props.text}
+    </ButtonDefault>
+  );
+};
 
 export default Button;
