8fca7e5ffe435f4dd1eec5d9cc6c220a34e7b61a
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-14 18:40:09 -0300

    refactor: improve indentation

--- to-do-app/src/hooks/useStorageData.ts
+++ to-do-app/src/hooks/useStorageData.ts
@@ -1,6 +1,6 @@
 export const getStorageData = () => {
   const savedItem = localStorage.getItem('tasks') as string;
   const parsedItem = JSON.parse(savedItem);
-  
+
   return parsedItem || [];
 };
